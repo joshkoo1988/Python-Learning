ceb54c72af3b506a2591f2ba08ba3cb6a9635c0d
Joshua Koo <joshkoo1988@gmail.com>
2025-01-07 09:11:41 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -10,5 +10,7 @@ spear_length = 2.0
 sword_area = area_of_circle(sword_length)
 spear_area = area_of_circle(spear_length)
 
-print(sword_area)
-print(spear_area)
+print(f"sword_length = {sword_length}")
+print(f"spear_length = {spear_length}")
+print(f"The area of a sword attack is {sword_area}")
+print(f"The area of a spear attack is {spear_area}")
